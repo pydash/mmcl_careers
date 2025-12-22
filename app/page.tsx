df3ec91b3447af23c09d2fc2ca2f8783423b7dd6
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2025-12-22 22:32:06 +0800

    Added content

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <main>
-        <h1>Welcome to MMCL Careers</h1>
-        <p>Your dream job awaits at MMCL Careers</p>
-        <div className="flex flex-col items-center justify-center py-2">
-          <Button variant="destructive" size="lg">
-            Explore Job Openings
+      <main className="h-dvh flex flex-col items-center justify-center gap-4">
+        <div className="flex flex-col items-center">
+          <h1 className="font-semibold text-2xl">Welcome to MMCL Careers</h1>
+        <p className="text-muted-foreground">Your dream job awaits here</p>
+        </div>
+        <div className="flex gap-4">
+          <Button variant="default" size="lg">
+            <Link href="/signup" prefetch>Get Started</Link>
+          </Button>
+          <Button variant="outline" size="lg">
+            <Link href="/login" prefetch>Login</Link>
           </Button>
         </div>
       </main>
