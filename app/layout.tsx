0e3dde57bf88e07194f84a6ea428f0e67c03a297
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2025-12-30 15:36:51 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} font-sans antialiased`}
-      >
+      <body className={`${inter.variable} font-sans antialiased`}>
         {children}
       </body>
     </html>
