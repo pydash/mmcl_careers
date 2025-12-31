2e13b02620e9ea571e4af34e9bc4c6e6028510a8
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2025-12-31 13:01:47 +0800

    format change

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -38,6 +38,6 @@ export function getDateTime(isoString: string): string {
     const hours = String(date.getUTCHours()).padStart(2, "0");
     const minutes = String(date.getUTCMinutes()).padStart(2, "0");
     const seconds = String(date.getUTCSeconds()).padStart(2, "0");
-    return `${month} ${day}, ${year} ${hours}-${minutes}-${seconds}`;
+    return `${month} ${day}, ${year} | ${hours}:${minutes}:${seconds}`;
   }
 }
