2aa54d74ddda8afb279b23120dab06589e5ac8f5
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-01-03 04:48:54 +0800

    update

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 // components/sidebar.tsx
 import Link from "next/link";
 import Image from "next/image";
-import { LayoutDashboard, User, Settings, LogOut } from "lucide-react";
+import { LogOut } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
