bbcc8cbe1ce109b285c0c104c71d6a7d0c38f9f9
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-01-03 04:50:36 +0800

    new service for jobs page

---
+++ services/applicant/jobs/jobs.service.ts
@@ -0,0 +1,11 @@
+export async function fetchJobs() {
+  const response = await fetch("/api/applicant/jobs", {
+    method: "GET",
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch jobs");
+  }
+  const data = await response.json();
+  return data;
+}
