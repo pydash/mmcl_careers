6a124d7a5e83f7ccb87620d2fc228527a51b7240
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-01-03 04:50:13 +0800

    added new properties

--- models/Job.ts
+++ models/Job.ts
@@ -1,9 +1,16 @@
 interface Job {
   id: string;
   title: string;
+  department: string;
+  employment_type: string;
   description: string;
-  posted_at: string;
+  requirements: string;
+  responsibilities: string;
+  salary_min: string;
+  salary_max: string;
+  is_active: boolean;
   expiry_date: string;
+  posted_by: string;
 }
 
 export default Job;
