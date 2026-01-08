3e25ee9e847518059f44a39b852222725769c042
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-01-08 08:01:21 +0800

    added query

---
+++ lib/queries/applicant/profile.ts
@@ -0,0 +1,18 @@
+export const APPLICANT_PROFILE_QUERY = `
+SELECT json_build_object(
+    'personal', to_jsonb(up),
+    'education', to_jsonb(eb),
+    'employment', json_agg(to_jsonb(emp)),
+    'license', json_agg(to_jsonb(lc)),
+    'extras', to_jsonb(ue),
+    'attachments', json_agg(to_jsonb(att))
+) AS profile
+FROM user_profiles up
+LEFT JOIN educational_backgrounds eb ON up.id = eb.id
+LEFT JOIN user_extras ue ON up.id = ue.id
+LEFT JOIN user_attachments att ON up.id = att.acc_id
+LEFT JOIN employment_histories emp ON up.id = emp.acc_id
+LEFT JOIN license_certifications lc ON up.id = lc.acc_id
+WHERE up.id = $1
+GROUP BY up.id, eb.id, ue.id
+`;
