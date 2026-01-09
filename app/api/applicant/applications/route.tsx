f361a227547e06c23a571b2328fb29ca37ec4653
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-01-10 07:43:04 +0800

    new api

---
+++ app/api/applicant/applications/route.tsx
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import db from "@/lib/db";
+import { APPLICATION_LIST_DETAILS_QUERY } from "@/lib/queries/applicant/applications/application_list_details";
+
+export async function GET(request: NextRequest) {
+  try {
+    const cookieStore = await cookies();
+    const userId = cookieStore.get("session_user_id")?.value;
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const jobs_query_result = await db
+      .query(APPLICATION_LIST_DETAILS_QUERY, [userId])
+      .then((res: any) => res.rows);
+
+    return NextResponse.json(jobs_query_result);
+  } catch (error) {
+    console.error("Error fetching jobs", error);
+    return NextResponse.json(
+      { error: "Failed to fetch jobs" },
+      { status: 500 }
+    );
+  }
+}
