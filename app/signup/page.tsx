f5f317782faab6d74c2d7db7605cac413014d611
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-01-22 16:27:20 +0800

    rearranged ui; enhancements

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -75,104 +75,105 @@ export default function SignupPage() {
         </div>
       )}
 
-      <main className="h-dvh flex flex-col items-center justify-center gap-4">
-        <div className="flex justify-center">
-          <Image
-            src="/MMCL_Logo_Horizontal.png"
-            alt="Signup Image"
-            width={150}
-            height={150}
-          />
-        </div>
-        <form>
-          <FieldGroup>
-            <FieldSet>
-              <FieldLegend>Sign Up</FieldLegend>
-              <FieldDescription>
-                Please enter your email to create an account
-              </FieldDescription>
-              <Field>
-                <FieldLabel>
-                  <Label htmlFor="email">Email</Label>
-                </FieldLabel>
-                <Input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Enter email address"
-                  value={formData.email}
-                  onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
-                  }
-                />
-              </Field>
-              <Field>
-                <FieldLabel>
-                  <Label htmlFor="password">Password</Label>
-                </FieldLabel>
-                <div className="relative">
+      <main className="min-h-dvh flex flex-col items-center justify-center bg-blue-950">
+        <div className="w-full max-w-md bg-white shadow-lg shadow-accent-foreground p-8">
+          <div className="flex justify-center mb-6">
+            <Image
+              src="/MMCL_Logo_Horizontal.png"
+              alt="Signup Image"
+              width={150}
+              height={150}
+            />
+          </div>
+          <form action={formAction}>
+            <FieldGroup>
+              <FieldSet>
+                <FieldLegend>Sign Up</FieldLegend>
+                <FieldDescription>
+                  Please enter your email to create an account
+                </FieldDescription>
+                <Field>
+                  <FieldLabel>
+                    <Label htmlFor="email">Email</Label>
+                  </FieldLabel>
                   <Input
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    name="password"
-                    placeholder="Enter password"
-                    value={formData.password}
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter email address"
+                    value={formData.email}
                     onChange={(e) =>
-                      setFormData({ ...formData, password: e.target.value })
+                      setFormData({ ...formData, email: e.target.value })
                     }
                   />
-                  <button
-                    type="button"
-                    onClick={() => setShowPassword((prev) => !prev)}
-                    className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
-                    tabIndex={-1}
-                  >
-                    {showPassword ? (
-                      <AiOutlineEye size={20} />
-                    ) : (
-                      <AiOutlineEyeInvisible size={20} />
-                    )}
-                  </button>
-                </div>
-              </Field>
+                </Field>
+                <Field>
+                  <FieldLabel>
+                    <Label htmlFor="password">Password</Label>
+                  </FieldLabel>
+                  <div className="relative">
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      id="password"
+                      name="password"
+                      placeholder="Enter password"
+                      value={formData.password}
+                      onChange={(e) =>
+                        setFormData({ ...formData, password: e.target.value })
+                      }
+                    />
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword((prev) => !prev)}
+                      className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                      tabIndex={-1}
+                    >
+                      {showPassword ? (
+                        <AiOutlineEye size={20} />
+                      ) : (
+                        <AiOutlineEyeInvisible size={20} />
+                      )}
+                    </button>
+                  </div>
+                </Field>
+                <Field>
+                  <FieldLabel>
+                    <Label htmlFor="confirm-password">Confirm Password</Label>
+                  </FieldLabel>
+                  <Input
+                    type="password"
+                    id="confirm-password"
+                    name="confirmPassword"
+                    placeholder="Enter password"
+                    value={formData.confirmPassword}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        confirmPassword: e.target.value,
+                      })
+                    }
+                  />
+                </Field>
+                <FieldSeparator />
+                <Button
+                  type="submit"
+                  variant="default"
+                  className="w-full bg-red-600 hover:bg-red-500"
+                >
+                  Create Account
+                </Button>
+              </FieldSet>
               <Field>
-                <FieldLabel>
-                  <Label htmlFor="confirm-password">Confirm Password</Label>
-                </FieldLabel>
-                <Input
-                  type="password"
-                  id="confirm-password"
-                  name="confirmPassword"
-                  placeholder="Enter password"
-                  value={formData.confirmPassword}
-                  onChange={(e) =>
-                    setFormData({
-                      ...formData,
-                      confirmPassword: e.target.value,
-                    })
-                  }
-                />
+                <FieldDescription className="text-center">
+                  Already have an account?{" "}
+                  <a href="/login" className="text-blue-600 hover:underline">
+                    Log in
+                  </a>
+                </FieldDescription>
               </Field>
-              <FieldSeparator />
-              <Button
-                type="submit"
-                variant="default"
-                size="lg"
-                className="w-full"
-              >
-                Create Account
-              </Button>
-            </FieldSet>
-            <Field>
-              <FieldDescription className="text-center">
-                Already have an account?{" "}
-                <a href="/login" className="text-blue-600 hover:underline">
-                  Log in
-                </a>
-              </FieldDescription>
-            </Field>
-          </FieldGroup>
-        </form>
+            </FieldGroup>
+          </form>
+        </div>
       </main>
     </>
   );
