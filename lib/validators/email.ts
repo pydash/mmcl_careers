7a6d43b91e5ce0e99fb0589bb6b0e4a78befa055
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-03-18 05:50:43 +0800

    Revert "Merge remote-tracking branch 'origin/master' into v3"
    This reverts commit b8cbff7793e25cf36360b0a3909fb7d26d287b25, reversing
    changes made to ff7b97b162ed68864fb4034aeaad3149cbbd5be0.

--- lib/validators/email.ts
+++
@@ -1,5 +0,0 @@
-export function validateEmail(email: string): boolean {
-  if (!email) return false;
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-}
--- lib/validators/password.ts
+++
@@ -1,5 +0,0 @@
-export function validatePassword(password: string): boolean {
-  if (!password) return false;
-  const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]{8,}$/;
-  return regex.test(password);
-}
