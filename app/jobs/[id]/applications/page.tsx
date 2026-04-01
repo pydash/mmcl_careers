9cffe23efc54ccb0c8e6b95ec214a098961a932f
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-04-01 18:18:33 +0800

    feat: add pages for specific and the applications

--- app/jobs/[id]/applications/page.tsx
+++ app/jobs/[id]/applications/page.tsx
@@ -1,17 +1,23 @@
 import { getUserRole } from "@/lib/auth";
 import HRJobApplications from "@/components/hr/jobs/applications/job-applications-page";
 import AdminJobApplications from "@/components/admin/jobs/applications/job-applications-page";
+import NotPermittedPage from "@/components/not-permitted-page";
 
-export default async function JobApplicationsPage() {
+export default async function JobApplicationsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const userRole = await getUserRole();
 
   if (userRole === "HR") {
-    return <HRJobApplications />;
+    return <HRJobApplications id={id} />;
   }
 
   if (userRole === "ADMIN") {
-    return <AdminJobApplications />;
+    return <AdminJobApplications id={id} />;
   }
 
-  return <div> Unauthorized</div>;
+  return <NotPermittedPage />;
 }
