73251c9a4cfe970aa418c40c47d4d5b738d5d5be
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-04-08 16:21:21 +0800

    chore: remove old folder formatters

--- utils/formatCurrency.ts
+++
@@ -1,9 +0,0 @@
-export function formatCurrency(amount: number): string {
-  const wholeNumber = Math.floor(amount);
-  return `₱${wholeNumber.toLocaleString()}`;
-}
-
-export function formatCurrencyShort(amount: number): string {
-  const thousands = Math.round(amount / 1000);
-  return `₱${thousands}k`;
-}
